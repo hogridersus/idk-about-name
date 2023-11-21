4e36578dafdc7a451bf1a6a47ed8c33f16bfcc3b
BuildTools <unconfigured@null.spigotmc.org>
2023-11-21 17:57:50 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import sys
 from random import randint
 
+from UI import Ui_MainWindow
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
 class Classwork(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
         self.qp = QPainter()
         self.flag = False
-        self.qbtn.clicked.connect(self.drawf)
+        self.ui.qbtn.clicked.connect(self.drawf)
 
     def drawf(self):
         self.flag = True
@@ -27,7 +28,7 @@ class Classwork(QMainWindow):
 
     def draw(self):
         R = randint(20, 400)
-        self.qp.setBrush(QColor(255, 255, 0))
+        self.qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         self.qp.drawEllipse(int(randint(0, 800) - R / 2),
                             int(randint(0, 580) - R / 2), R, R)
 
